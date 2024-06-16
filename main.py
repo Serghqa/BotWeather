ba3e9245d1a9afa1d7df493c3d1eb7d651c7fe87
Sergey Ponomarev <psl.ru@mail.ru>
2024-06-16 19:37:18 +0500

    BotWeather 1.0

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
 import asyncio
 from aiohttp import ClientSession
-from api import api
+
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
+
+from config import TOKEN, KEY_WEATHER
+
+
+bot = Bot(TOKEN)
+dp = Dispatcher()
+
+start_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Perm')]
+    ],
+    resize_keyboard=True
+)
 
 
 async def get_weather(city):
     async with ClientSession() as session:
         url = f'http://api.openweathermap.org/data/2.5/weather'
-        params = {'q': city, 'appid': api}
+        params = {'q': city, 'appid': KEY_WEATHER}
         async with session.get(url=url, params=params) as response:
             weather_json = await response.json()
-            print(f'{city}: {int(weather_json['main']['temp']-273.15)}')
+            answer = f'{city}: {int(weather_json['main']['temp']-273.15)}'
+            return answer
 
 
-async def main(city):
-    task = asyncio.create_task(get_weather(city))
-    await task
+@dp.message(Command('start'))
+async def process_start_command(message: Message):
+    await message.answer('Погода в Перми', reply_markup=start_kb)
 
 
-asyncio.run(main('Perm'))
+@dp.message(F.text=='Perm')
+async def get_weather_answer(message: Message):
+    answer = await asyncio.create_task(get_weather('Perm'))
+    await message.answer(text=answer)
+
+
+dp.run_polling(bot)

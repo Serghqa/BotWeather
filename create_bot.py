4d09df011f4422947b6fd5e6fa757c50c45b87ae
Sergey Ponomarev <psl.ru@mail.ru>
2024-06-25 20:38:46 +0500

    add .env, rename main -> create_bot

--- create_bot.py
+++ create_bot.py
@@ -1,14 +1,21 @@
+import os
+
 import asyncio
+
 from aiohttp import ClientSession
 
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 
-from config import TOKEN, KEY_WEATHER
+from dotenv import load_dotenv
 
 
-bot = Bot(TOKEN)
+load_dotenv()
+token = os.getenv('TOKEN')
+key_weather = os.getenv('KEY_WEATHER')
+
+bot = Bot(token)
 dp = Dispatcher()
 
 start_kb = ReplyKeyboardMarkup(
@@ -21,11 +28,17 @@ start_kb = ReplyKeyboardMarkup(
 
 async def get_weather(city):
     async with ClientSession() as session:
-        url = f'http://api.openweathermap.org/data/2.5/weather'
-        params = {'q': city, 'appid': KEY_WEATHER}
+        url = 'http://api.openweathermap.org/data/2.5/weather'
+        params = {'q': city, 'appid': key_weather, 'units': 'metric'}
         async with session.get(url=url, params=params) as response:
             weather_json = await response.json()
-            answer = f'{city}: {int(weather_json['main']['temp']-273.15)}'
+            temp = f'{city}: {int(weather_json['main']['temp'])}'
+            temp_feel = f'{int(weather_json['main']['feels_like'])}'
+            press = f'{int(weather_json['main']['pressure'])}'
+            humidity = f'{int(weather_json['main']['humidity'])}'
+            answer = f'Температура {temp}\nПо ощущениям {temp_feel}\n'\
+                f'Давление {press}\nВлажность {humidity}'
+
             return answer
 
 
